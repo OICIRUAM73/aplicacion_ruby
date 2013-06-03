3d4c718e0e92872ea81a76861a610fa7f067d5a8
mauricio <mauricio@mauricio-mint.(none)>
2013-06-02 19:47:30 -0500

    Created a rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiApp::Application.config.secret_token = '3f3e018064b4e1e81a79f4fd3432c81919606a25bca87893f04a622736318c4e15a00f8e12922518e361ee2fd21ab8b872c1c435b3ff6383bb3812101575e5b2'
